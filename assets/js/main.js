24d350400e144040b0850b4086b485c3d24791a8
kseniya <kseniya.for.email@gmail.com>
2021-02-08 14:58:51 +0200

    Portfolio was added

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,3 +71,27 @@ function scrollTop() {
 }
 
 window.addEventListener("scroll", scrollTop);
+
+//mixup filter portfolio
+
+const mixer = mixitup(".portfolio__container", {
+    selectors: {
+        target: ".portfolio__content"
+    },
+    animation: {
+        duration: 400
+    }
+});
+
+// link active portfolio
+
+const linkPortfolio = document.querySelectorAll(".portfolio__item");
+
+function activePortfolio() {
+    if (linkPortfolio) {
+        linkPortfolio.forEach(l => l.classList.remove("active-portfolio"));
+        this.classList.add("active-portfolio");
+    }
+}
+
+linkPortfolio.forEach(l => l.addEventListener("click", activePortfolio));
